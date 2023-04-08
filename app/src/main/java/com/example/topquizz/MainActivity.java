fa1e2283084c49199a12ccc916a272a1ba4fc813
khali <khalifalo178@gmail.com>
2023-04-08 18:41:20 +0200

    Set new content

--- app/src/main/java/com/example/topquizz/MainActivity.java
+++ app/src/main/java/com/example/topquizz/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.topquizz;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mGreetingTextView;
+    private EditText mNameEditText;
+    private Button mPlayButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGreetingTextView = findViewById(R.id.main_textview_greeting);
+        mNameEditText = findViewById(R.id.main_edittext_name);
+        mPlayButton = findViewById(R.id.main_button_play);
+
+        mPlayButton.setEnabled(false);
+
+        mNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // Activera le bouton en fonction de la longueur de la chaîne de caractère
+                mPlayButton.setEnabled(!editable.toString().isEmpty());
+
+            }
+        });
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
